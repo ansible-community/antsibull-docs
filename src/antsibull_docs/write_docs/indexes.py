7094316875f6fda30f64494be8fbbc988ea696a1
Felix Fontein <felix@fontein.de>
2022-12-27 14:44:14 +0100

    Show callback type, and render callback type indexes.

--- src/antsibull_docs/write_docs/indexes.py
+++ src/antsibull_docs/write_docs/indexes.py
@@ -24,6 +24,46 @@ from . import PluginCollectionInfoT, _render_template
 mlog = log.fields(mod=__name__)
 
 
+async def write_callback_type_index(callback_type: str,
+                                    per_collection_plugins: t.Mapping[str, t.Mapping[str, str]],
+                                    collection_metadata: t.Mapping[str, AnsibleCollectionMetadata],
+                                    template: Template,
+                                    dest_filename: str,
+                                    for_official_docsite: bool = False) -> None:
+    """
+    Write an index page for each plugin type.
+
+    :arg callback_type: The callback plugin type to write the index for.
+    :arg per_collection_plugins: Mapping of collection_name to Mapping of plugin_name to
+        short_description.
+    :arg collection_metadata: Dictionary mapping collection names to collection metadata objects.
+    :arg template: A template to render the plugin index.
+    :arg dest_filename: The destination filename.
+    :kwarg for_official_docsite: Default False.  Set to True to use wording specific for the
+        official docsite on docs.ansible.com.
+    """
+    public_per_collection_plugins = {}
+    for collection_name, plugins in per_collection_plugins.items():
+        public_plugins = {}
+        collection_meta = collection_metadata[collection_name]
+        private_plugins = collection_meta.private_plugins.get('callback') or []
+        for plugin_name, plugin_data in plugins.items():
+            if plugin_name not in private_plugins:
+                public_plugins[plugin_name] = plugin_data
+        if public_plugins:
+            public_per_collection_plugins[collection_name] = public_plugins
+
+    index_contents = _render_template(
+        template,
+        dest_filename,
+        callback_type=callback_type,
+        per_collection_plugins=public_per_collection_plugins,
+        for_official_docsite=for_official_docsite,
+    )
+
+    await write_file(dest_filename, index_contents)
+
+
 async def write_plugin_type_index(plugin_type: str,
                                   per_collection_plugins: t.Mapping[str, t.Mapping[str, str]],
                                   template: Template,
@@ -49,6 +89,55 @@ async def write_plugin_type_index(plugin_type: str,
     )
 
     await write_file(dest_filename, index_contents)
+
+
+async def output_callback_indexes(plugin_info: PluginCollectionInfoT,
+                                  collection_metadata: t.Mapping[str, AnsibleCollectionMetadata],
+                                  dest_dir: str,
+                                  collection_url: CollectionNameTransformer,
+                                  collection_install: CollectionNameTransformer,
+                                  for_official_docsite: bool = False) -> None:
+    """
+    Generate top-level callback plugin index pages for all callback plugins of a type in all
+    collections.
+
+    :arg plugin_info: Mapping of callback_type to Mapping of collection_name to Mapping of
+        plugin_name to short_description.
+    :arg collection_metadata: Dictionary mapping collection names to collection metadata objects.
+    :arg dest_dir: The directory to place the documentation in.
+    :kwarg for_official_docsite: Default False.  Set to True to use wording specific for the
+        official docsite on docs.ansible.com.
+    """
+    flog = mlog.fields(func='output_callback_indexes')
+    flog.debug('Enter')
+
+    env = doc_environment(
+        ('antsibull_docs.data', 'docsite'),
+        collection_url=collection_url,
+        collection_install=collection_install)
+    # Get the templates
+    plugin_list_tmpl = env.get_template('list_of_callback_plugins.rst.j2')
+
+    collection_toplevel = os.path.join(dest_dir, 'collections')
+    flog.fields(toplevel=collection_toplevel, exists=os.path.isdir(collection_toplevel)).debug(
+        'collection_toplevel exists?')
+    # This is only safe because we made sure that the top of the directory tree we're writing to
+    # (docs/docsite/rst) is only writable by us.
+    os.makedirs(collection_toplevel, mode=0o755, exist_ok=True)
+
+    writers = []
+    lib_ctx = app_context.lib_ctx.get()
+    async with asyncio_pool.AioPool(size=lib_ctx.thread_max) as pool:
+        for callback_type, per_collection_data in plugin_info.items():
+            filename = os.path.join(collection_toplevel, f'callback_index_{callback_type}.rst')
+            writers.append(await pool.spawn(
+                write_callback_type_index(
+                    callback_type, per_collection_data, collection_metadata, plugin_list_tmpl,
+                    filename, for_official_docsite=for_official_docsite)))
+
+        await asyncio.gather(*writers)
+
+    flog.debug('Leave')
 
 
 async def output_plugin_indexes(plugin_info: PluginCollectionInfoT,
