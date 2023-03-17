406cacd56f9305e23dd1040f1d58d2b2bab15765
Felix Fontein <felix@fontein.de>
2023-03-17 21:12:57 +0100

    Fix typing for Python 3.6 and 3.7.

--- src/antsibull_docs/markup/dom.py
+++ src/antsibull_docs/markup/dom.py
@@ -9,11 +9,16 @@ DOM classes used by parser.
 
 from enum import Enum
 from typing import NamedTuple
+import sys
 
 import typing as t
 
 
-ErrorType = t.Union[t.Literal['ignore'], t.Literal['message'], t.Literal['exception']]
+if sys.version_info >= (3, 8):
+    ErrorType = t.Union[t.Literal['ignore'], t.Literal['message'], t.Literal['exception']]
+else:
+    # Python 3.6/3.7 do not have t.Literal
+    ErrorType = str
 
 
 class PluginIdentifier(NamedTuple):
@@ -40,54 +45,54 @@ class PartType(Enum):
 
 
 class TextPart(NamedTuple):
-    type: t.Literal[PartType.TEXT]
+    type: 't.Literal[PartType.TEXT]'
     text: str
 
 
 class ItalicPart(NamedTuple):
-    type: t.Literal[PartType.ITALIC]
+    type: 't.Literal[PartType.ITALIC]'
     text: str
 
 
 class BoldPart(NamedTuple):
-    type: t.Literal[PartType.BOLD]
+    type: 't.Literal[PartType.BOLD]'
     text: str
 
 
 class ModulePart(NamedTuple):
-    type: t.Literal[PartType.MODULE]
+    type: 't.Literal[PartType.MODULE]'
     fqcn: str
 
 
 class PluginPart(NamedTuple):
-    type: t.Literal[PartType.PLUGIN]
+    type: 't.Literal[PartType.PLUGIN]'
     plugin: PluginIdentifier
 
 
 class URLPart(NamedTuple):
-    type: t.Literal[PartType.URL]
+    type: 't.Literal[PartType.URL]'
     url: str
 
 
 class LinkPart(NamedTuple):
-    type: t.Literal[PartType.LINK]
+    type: 't.Literal[PartType.LINK]'
     text: str
     url: str
 
 
 class RSTRefPart(NamedTuple):
-    type: t.Literal[PartType.RST_REF]
+    type: 't.Literal[PartType.RST_REF]'
     text: str
     ref: str
 
 
 class CodePart(NamedTuple):
-    type: t.Literal[PartType.CODE]
+    type: 't.Literal[PartType.CODE]'
     text: str
 
 
 class OptionNamePart(NamedTuple):
-    type: t.Literal[PartType.OPTION_NAME]
+    type: 't.Literal[PartType.OPTION_NAME]'
     plugin: t.Optional[PluginIdentifier]
     link: t.List[str]
     name: str
@@ -95,17 +100,17 @@ class OptionNamePart(NamedTuple):
 
 
 class OptionValuePart(NamedTuple):
-    type: t.Literal[PartType.OPTION_VALUE]
+    type: 't.Literal[PartType.OPTION_VALUE]'
     value: str
 
 
 class EnvVariablePart(NamedTuple):
-    type: t.Literal[PartType.ENV_VARIABLE]
+    type: 't.Literal[PartType.ENV_VARIABLE]'
     name: str
 
 
 class ReturnValuePart(NamedTuple):
-    type: t.Literal[PartType.RETURN_VALUE]
+    type: 't.Literal[PartType.RETURN_VALUE]'
     plugin: t.Optional[PluginIdentifier]
     link: t.List[str]
     name: str
@@ -113,11 +118,11 @@ class ReturnValuePart(NamedTuple):
 
 
 class HorizontalLinePart(NamedTuple):
-    type: t.Literal[PartType.HORIZONTAL_LINE]
+    type: 't.Literal[PartType.HORIZONTAL_LINE]'
 
 
 class ErrorPart(NamedTuple):
-    type: t.Literal[PartType.ERROR]
+    type: 't.Literal[PartType.ERROR]'
     message: str
 
 
