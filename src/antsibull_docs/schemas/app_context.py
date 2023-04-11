bdf889a3714286e8604f4ddbe32a9a670bd99e52
Felix Fontein <felix@fontein.de>
2023-04-11 06:14:35 +0200

    Fix comment.

--- src/antsibull_docs/schemas/app_context.py
+++ src/antsibull_docs/schemas/app_context.py
@@ -15,7 +15,7 @@ import pydantic as p
 from antsibull_core.schemas.context import AppContext as CoreAppContext
 from antsibull_core.schemas.validators import convert_bool
 
-#: Valid choices for a logging level field
+#: Valid choices for the docs parsing backend
 DOC_PARSING_BACKEND_CHOICES_F = p.Field(
     'auto', regex='^(auto|ansible-core-2\\.13)$')
 
