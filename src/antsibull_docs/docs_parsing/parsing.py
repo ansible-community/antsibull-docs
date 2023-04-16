cce508323211a4736aaaf5db651ed01c5aa87737
Felix Fontein <felix@fontein.de>
2023-04-16 14:49:32 +0200

    Use doc_parsing_backend from app_ctx and not lib_ctx.

--- src/antsibull_docs/docs_parsing/parsing.py
+++ src/antsibull_docs/docs_parsing/parsing.py
@@ -51,9 +51,9 @@ async def get_ansible_plugin_info(venv: t.Union['VenvRunner', 'FakeVenvRunner'],
     """
     flog = mlog.fields(func='get_ansible_plugin_info')
 
-    lib_ctx = app_context.lib_ctx.get()
+    app_ctx = app_context.app_ctx.get()
 
-    doc_parsing_backend = lib_ctx.doc_parsing_backend
+    doc_parsing_backend = app_ctx.doc_parsing_backend
     if doc_parsing_backend == 'auto':
         version = get_ansible_core_version(venv)
         flog.debug(f'Ansible-core version: {version}')
