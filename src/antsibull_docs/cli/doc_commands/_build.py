a58b87642f5045569bfcca06d696107e69d655a6
Felix Fontein <felix@fontein.de>
2023-05-06 21:08:42 +0200

    Pass collection metadata to more places, and improve plugin index templates (#144)
    * Refactoring to pass collection metadata through to more places.
    
    * Add 'no plugins found' message to plugin index templates.

--- src/antsibull_docs/cli/doc_commands/_build.py
+++ src/antsibull_docs/cli/doc_commands/_build.py
@@ -170,7 +170,7 @@ def generate_docs_for_all_collections(venv: VenvRunner | FakeVenvRunner,
                                                         breadcrumbs=breadcrumbs,
                                                         for_official_docsite=for_official_docsite))
         flog.notice('Finished writing collection namespace index')
-        asyncio.run(output_plugin_indexes(plugin_contents, dest_dir,
+        asyncio.run(output_plugin_indexes(plugin_contents, collection_metadata, dest_dir,
                                           collection_url=collection_url,
                                           collection_install=collection_install,
                                           for_official_docsite=for_official_docsite))
