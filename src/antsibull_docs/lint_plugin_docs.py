6db24d056d01bd55578ca7208b934d4eda222a3a
Felix Fontein <felix@fontein.de>
2023-06-09 22:50:46 +0200

    Fix duplicate detection. (#160)

--- src/antsibull_docs/lint_plugin_docs.py
+++ src/antsibull_docs/lint_plugin_docs.py
@@ -602,6 +602,7 @@ def lint_collection_plugin_docs(
                 dependency = dependencies.pop(0)
                 if dependency in done_dependencies:
                     continue
+                done_dependencies.add(dependency)
                 dep_namespace, dep_name = dependency.split(".", 2)
                 dep_collection_path = collection_finder.find(dep_namespace, dep_name)
                 if dep_collection_path:
