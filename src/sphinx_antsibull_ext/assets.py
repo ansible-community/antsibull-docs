994c7ef0201e99982dde48cddea7daa3ce94383a
Felix Fontein <felix@fontein.de>
2023-08-20 10:24:54 +0200

    Support more HTML-based builders.
    Our stylesheet might not be perfect for them, like the collection links for
    epub targets, but at least the other things are styled appropriately, and
    there is no error about a missing stylesheet.

--- src/sphinx_antsibull_ext/assets.py
+++ src/sphinx_antsibull_ext/assets.py
@@ -23,8 +23,38 @@ BUILDER_FILES = {
     },
 }
 
+# A map mapping builder names to the builder they inherit (indirectly) from.
+# Mainly necessary to link the myriad of HTML-derived builders to the main HTML
+# builder, to make sure that the stylesheet is copied in all these cases.
+_BUILDER_ALIASES = {
+    "applehelp": "html",
+    "devhelp": "html",
+    "dirhtml": "html",
+    "epub": "html",
+    "htmlhelp": "html",
+    "qthelp": "html",
+    "singlehtml": "html",
+    "websupport": "html",
+}
 
-def _copy_asset_files(app, exc):  # pylint: disable=unused-argument
+# A list of builders which needs the static files to be presented before the
+# build finishes. I only verified this for epub, since the others also provide
+# similar formats my guess is that they also need it.
+_BUILDER_COPY_ON_INITED = [
+    "applehelp",
+    "devhelp",
+    "epub",
+    "htmlhelp",
+    "qthelp",
+    "websupport",
+]
+
+for _builder_name, _alias in _BUILDER_ALIASES.items():
+    if _builder_name not in BUILDER_FILES and _alias in BUILDER_FILES:
+        BUILDER_FILES[_builder_name] = BUILDER_FILES[_alias]
+
+
+def _copy_asset_files(app):
     """
     Copy asset files.
     """
@@ -41,12 +71,38 @@ def _copy_asset_files(app, exc):  # pylint: disable=unused-argument
             f.write(data)
 
 
+def _copy_asset_files_inited(app):
+    # pylint: disable=line-too-long
+    """
+    Copy asset files on 'builder-inited' signal.
+
+    While the official documentation says that stylesheets should be copied
+    during the build-finished signal
+    (https://www.sphinx-doc.org/en/master/development/theming.html#add-your-own-static-files-to-the-build-assets)
+    this won't work for builders such as epub, since they assemble the epub
+    file *before* the build finishes. Since there is no other documented
+    signal when to copy over the files, I picked the 'builder-inited' signal.
+    """
+    # pylint: enable=line-too-long
+    if app.builder.name in _BUILDER_COPY_ON_INITED:
+        _copy_asset_files(app)
+
+
+def _copy_asset_files_finished(app, exc):  # pylint: disable=unused-argument
+    """
+    Copy asset files on 'build-finished' signal.
+    """
+    if app.builder.name not in _BUILDER_COPY_ON_INITED:
+        _copy_asset_files(app)
+
+
 def setup_assets(app):
     """
     Setup assets for a Sphinx app object.
     """
     # Copy assets
-    app.connect("build-finished", _copy_asset_files)
+    app.connect("builder-inited", _copy_asset_files_inited)
+    app.connect("build-finished", _copy_asset_files_finished)
 
     # Add CSS files
     for file in BUILDER_FILES.get("html", {}):
