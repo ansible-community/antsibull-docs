1e5521e5ada47fb0ac88a5f681173d570f8c167e
Felix Fontein <felix@fontein.de>
2025-06-24 21:40:20 +0200

    Post-release version bump.

--- src/antsibull_docs/__init__.py
+++ src/antsibull_docs/__init__.py
@@ -4,7 +4,7 @@
 # SPDX-FileCopyrightText: 2020, Ansible Project
 """The main antsibull-docs module. Contains versioning information."""
 
-__version__ = "2.18.0"
+__version__ = "2.18.0.post0"
 
 
 __all__ = ("__version__",)
