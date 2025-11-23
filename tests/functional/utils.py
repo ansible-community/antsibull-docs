04aa95521e2d5d45732555f402a31178bfbf7601
Felix Fontein <felix@fontein.de>
2025-11-23 14:55:52 +0100

    Refactor tests.

--- tests/functional/utils.py
+++ tests/functional/utils.py
@@ -4,6 +4,7 @@
 
 from __future__ import annotations
 
+import difflib
 import os
 from contextlib import contextmanager
 from unittest import mock
@@ -46,3 +47,66 @@ def update_environment(environment: dict[str, str | None]) -> None:
             os.environ[k] = backup[k]
         elif k in os.environ:
             del os.environ[k]
+
+
+def scan_directories(root: os.PathLike) -> dict[str, tuple[str, list[str]]]:
+    result = {}
+    for path, dirs, files in os.walk(root):
+        result[os.path.relpath(path, root)] = (path, files)
+    return result
+
+
+def compare_files(source: os.PathLike, dest: os.PathLike, path: str) -> int:
+    with open(source) as f:
+        src = f.read()
+    with open(dest) as f:
+        dst = f.read()
+    if src == dst:
+        return 0
+    for line in difflib.unified_diff(src.splitlines(), dst.splitlines(), path, path):
+        if line[0] == "@":
+            print(line)
+        elif line[0] == "-":
+            print(f"\033[41m\033[9m{line}\033[29m\033[49m")
+        elif line[0] == "+":
+            print(f"\033[42m{line}\033[49m")
+        else:
+            print(line)
+    return 1
+
+
+def compare_directories(
+    source: dict[str, tuple[str, list[str]]], dest: dict[str, tuple[str, list[str]]]
+) -> bool:
+    differences = 0
+    for path in source:
+        if path not in dest:
+            print(f"Directory {path} exists only in the baseline!")
+            differences += 1
+            continue
+        source_files = set(source[path][1])
+        dest_files = set(dest[path][1])
+        for file in source_files:
+            if file not in dest_files:
+                differences += 1
+                print(f"File {os.path.join(path, file)} exists only in the baseline!")
+                continue
+            source_path = os.path.join(source[path][0], file)
+            dest_path = os.path.join(dest[path][0], file)
+            differences += compare_files(
+                source_path, dest_path, os.path.join(path, file)
+            )
+        for file in dest_files:
+            if file not in source_files:
+                differences += 1
+                print(
+                    f"File {os.path.join(path, file)} exists only in the generated result!"
+                )
+    for path in dest:
+        if path not in source:
+            print(f"Directory {path} exists only in the generated result!")
+            differences += 1
+            continue
+    if differences:
+        print(f"Found {differences} differences.")
+    assert differences == 0
